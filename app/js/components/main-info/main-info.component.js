eb000b427bae54808d211bfbe3b33f9f1c76d363
Pavlo <p.bazyliuk@gmail.com>
2017-01-29 17:10:05 +0200

    main page styled

--- app/js/components/main-info/main-info.component.js
+++ app/js/components/main-info/main-info.component.js
@@ -7,9 +7,52 @@
 
 	function controller() {
 
-			var model = this;
+		var model = this;
+			
+		$(".gallery__next-btn").on('click', function() {
+			var firstEl = $(".gallery__images img:first-child");
+			firstEl.addClass("modal");
+			//firstEl.trigger("click");
+
+			$(firstEl).remove().appendTo(".gallery__images");
+
+		});
+
+		$(".gallery__prev-btn").on('click', function() {
+			var lastEl = $(".gallery__images img:last-child");
+			lastEl.addClass("modal");
+			$(lastEl).remove().prependTo(".gallery__images");
+
+		});
+
+
+		$(document).on('click', '.gallery__item', showModal);
 		
+		 function showModal() {
+		  $("#modal").addClass("isShown");
+		  var cur = this;
+		  /*console.log(cur)*/
+		  $(document).off();
+		  $( cur ).clone().toggleClass("gallery__item").appendTo( $( ".modal-window__image-wrapper" ) );
 		}
+
+
+		$(".modal-window__close").on("click", function() {
+		  $("#modal").toggleClass("isShown");
+		  $(".modal-window__image-wrapper").empty();
+		  $(document).on('click', '.gallery__item', showModal);
+		});
+
+		window.onclick = function(event) {
+		  var modal = $("#modal")[0];
+		  // console.log(modal)
+		    if (event.target == modal) {
+		        $("#modal").toggleClass("isShown");
+		  		$(".modal-window__image-wrapper").empty();
+		  		$(document).on('click', '.gallery__item', showModal);
+		    }
+		}
+	}
 		
 	module.component("mainInfo", {
 
--- app/js/module.js
+++ app/js/module.js
@@ -7,24 +7,13 @@
 	  //$locationProvider.html5Mode(true);
 	   /* $locationProvider.hashPrefix('');*/
    		$routeProvider
-   			.when("/main", { template:"<span>main</span>"})
-   			.when("/about", { template:"<span>about</span>"})
+   			.when("/main", { template:"<main-info>main</main-info>"})
+   			.when("/about", { template:"<about-info>about</about-info>"})
    			.when("/cafe", { template:"<cafe-info></cafe-info>"})
    			.when("/menu", { template:"<menu-info></menu-info>"})
    			.when("/order", { template:"<order-info></order-info>"})
    			.when("/contacts", { template:"<contacts-info></contacts-info>"})
    			.otherwise({redirectTo:'/main'});
   	}]);
-
-
-    
-    //module.value("$routerRootComponent", "pastryApp");
-        
-   /* module.component("aboutInfo",{
-       template: "This is the about page" 
-    });*/
-
-
-  
 
 }());
